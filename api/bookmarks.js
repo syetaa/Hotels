affad9c23d2ee68232061af8f4e66a3ca77f4e0f
Michel <soinovmich@yandex.ru>
2024-05-08 20:33:47 +0300

    08.05.3

---
+++ api/bookmarks.js
@@ -0,0 +1,49 @@
+export default async function getBookmarks(token) {
+    const url = 'http://127.0.0.1:8000/bookmarks/';
+
+    const request = {
+        'method': 'GET',
+        headers: {
+            'Authorization': 'Bearer ' + token,
+        }
+    }
+
+    console.log(request);
+    
+    const response = await fetch(url, request);
+    if (response.status === 401) {
+        return false
+    }
+    const data = await response.json();
+    console.log(data);
+
+    if (data) {
+        return data;
+    }
+
+    return Promise.reject('Failed to get bookmarks from backend')
+
+}
+
+export async function addBookmark(id) {
+    const url = 'http://127.0.0.1:8000/bookmarks/';
+
+    const response = await fetch(url, {
+        'method': 'GET',
+        headers: {
+            'Authorization': 'Bearer ' + token,
+        }
+    });
+    if (response.status === 401) {
+        return false
+    }
+    const data = await response.json();
+    console.log(data);
+
+    if (data) {
+        return data;
+    }
+
+    return Promise.reject('Failed to get bookmarks from backend')
+
+}
