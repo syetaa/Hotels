8d725673c25698ad841491285d831ba661119a92
Michel <soinovmich@yandex.ru>
2024-05-10 10:44:10 +0300

    10.05.1

--- components/now_favorite/Bookmarks.jsx
+++ components/now_favorite/Bookmarks.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import styles from "@/components/now_favorite/Now_favorite.module.css";
+import styles from "./Bookmarks.module.css";
 import Image from "next/image";
 import hotels_card2 from "@/public/hotels_card2.png";
 import {FiHeart} from "react-icons/fi";
@@ -11,48 +11,48 @@ import {useEffect, useState} from "react";
 function BookmarkCard({bookmark}) {
     return (
         <div className={styles.card}>
-            <a href={`/rooms/${bookmark.room_id}`}>
-                <div className={styles.img}>
-                    <Image src={hotels_card2}/>
-                </div>
-                <div className={styles.card_info}>
-                    <div className={styles.cont1}>
-                        <div className={styles.text1}>
-                            {bookmark.hotel}
+                <a href={`/rooms/${bookmark.room_id}`}>
+                    <div className={styles.img}>
+                        <Image src={hotels_card2} />
+                    </div>
+                    <div className={styles.card_info}>
+                        <div className={styles.cont1}>
+                            <div className={styles.text1}>
+                                {bookmark.hotel}
+                            </div>
+                            <div className={styles.text2}>
+                                <div className={styles.text2_1}>
+                                    {bookmark.city}
+                                </div>
+                                <div className={styles.text2_2}>
+                                    -
+                                </div>
+                                <div className={styles.text2_3}>
+                                    1 км до центра
+                                </div>
+                            </div>
+                            <div className={styles.text3}>
+                                <div className={styles.text3_1}>
+                                    {bookmark.price}
+                                </div>
+                                <div className={styles.text3_2}>
+                                    8280 ₽ / ночь
+                                </div>
+                            </div>
+                            
                         </div>
-                        <div className={styles.text2}>
-                            <div className={styles.text2_1}>
-                                {bookmark.city}
+                        <div className={styles.cont2}>
+                            <div className={styles.like}>
+                                <FiHeart size={20} />
                             </div>
-                            <div className={styles.text2_2}>
-                                -
-                            </div>
-                            <div className={styles.text2_3}>
-                                1 км до центра
-                            </div>
-                        </div>
-                        <div className={styles.text3}>
-                            <div className={styles.text3_1}>
-                                {bookmark.price}
-                            </div>
-                            <div className={styles.text3_2}>
-                                8280 ₽ / ночь
+                            <div className={styles.score}>
+                                Бронь
                             </div>
                         </div>
                     </div>
-                    <div className={styles.cont2}>
-                        <div className={styles.score}>
-                            4.5
-                        </div>
-                        <div className={styles.like}>
-                            <FiHeart size={20}/>
-                        </div>
-
-                    </div>
-                </div>
-            </a>
-
-        </div>
+                </a>
+                
+            </div>
     )
 }
 
@@ -84,7 +84,7 @@ export default function Bookmarks({token}) {
                     </div>
                 </div> :
                 bookmarks.map(bookmark => (
-                    <div key={bookmark.id}>
+                    <div key={bookmark.id} className={styles.textmain}>
                         <BookmarkCard bookmark={bookmark}/>
                     </div>
                 ))}
